298c83827b314e7bd8c9967a2fea3f396674df38
Njokimary <marynjoki738@gmail.com>
2023-10-19 12:17:35 +0300

    feat:create classes

--- test.py
+++ test.py
@@ -8,3 +8,14 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)###created an instance of the Flask class
 
 app.config['SQLALCHEMY_DATABASE_URL'] = "sqlite:///app.db"##allows us to creat a database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONW'] = False ##used ro reduce the memory usage
+
+db = SQLAlchemy(app) ## create a db variable and add an instance app.SQLAlchemy is used to make queries to the database
+
+#create classes
+class TODO(db.Model):
+    id = db.Column(db.Interger, primary_key=True, index =True)
+    name = db.Column(db.String(32),nullable = False)
+    description = db.Column(db.String(32), nullable = False)
+
+db.create_all()
